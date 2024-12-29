3faba7d7181f9586cb2659facac8e324dd71f5ae
Noah Leibovitz <noah.leibovitz@outlook.com>
2024-12-28 22:01:29 -0800

    Creating Landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-
+import Landing from './Landing'
 import './App.css'
 
 function App() {
@@ -6,6 +6,7 @@ function App() {
   return (
     <>
       <h1>Hello World</h1>
+      <Landing />
     </>
   )
 }
